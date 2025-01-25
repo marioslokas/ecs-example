94ff1fc8dd16a471186d0e3d66e3332e9ffffc03
Marios-4 <marioslokas@gmail.com>
2025-01-25 20:08:44 +0100

    [update] changed spawner system to try out jobs

--- Assets/Scripts/SpawnerSystem.cs
+++ Assets/Scripts/SpawnerSystem.cs
@@ -1,19 +1,73 @@
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Burst;
-using UnityEngine;
 
+// spawner with jobs
+[BurstCompile]
+public partial struct SpawnerSystemOptimized : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        EntityCommandBuffer.ParallelWriter commandBuffer = GetEntityCommandBuffer(ref state);
+        new ProcessSpawnerJob
+        {
+            elapsedTime = SystemAPI.Time.ElapsedTime,
+            Ecb = commandBuffer
+        }.ScheduleParallel();
+    }
+
+    private EntityCommandBuffer.ParallelWriter GetEntityCommandBuffer(ref SystemState state)
+    {
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        return ecb.AsParallelWriter();
+    }
+}
+
+[BurstCompile]
+public partial struct ProcessSpawnerJob : IJobEntity
+{
+    public EntityCommandBuffer.ParallelWriter Ecb;
+    public double elapsedTime;
+
+    private void Execute([ChunkIndexInQuery] int chuckIndex, ref Spawner spawner)
+    {
+        bool canSpawn = elapsedTime > spawner.NextSpawnTime;
+        
+        if (canSpawn)
+        {
+            // spawn entity
+            var newEntity = Ecb.Instantiate(chuckIndex, spawner.spawnPrefab);
+
+            var transform = LocalTransform.FromPosition(spawner.SpawnPosition);
+            Ecb.SetComponent(chuckIndex, newEntity, transform);
+            spawner.NextSpawnTime = (float) elapsedTime + spawner.SpawnRate;
+        }
+    }
+}
+
+
+/*
+// spawner system without jobs
 [BurstCompile]
 public partial struct SpawnerSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
-        
     }
 
     public void OnDestroy(ref SystemState state)
     {
-        
     }
 
     [BurstCompile]
@@ -35,12 +89,12 @@ public partial struct SpawnerSystem : ISystem
         {
             // spawn entity
             var newEntity = state.EntityManager.Instantiate(spawner.ValueRO.spawnPrefab);
-            
+
             // set position
             Unity.Transforms.LocalTransform spawnTransform = LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition);
             state.EntityManager.SetComponentData(newEntity, spawnTransform);
 
-            spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+            spawner.ValueRW.NextSpawnTime = (float) SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
         }
     }
-}
+}*/
